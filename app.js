3f364d2e586d53931568d39b5cd8a187a9f62740
ndavidj <ndavidj@hotmail.com.ar>
2022-04-13 19:58:30 -0300

    repo en heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/public")))
 
 app.listen( process.env.PORT || 3030, () => 
-    {console.log("Servidor corriendo")})
+    {console.log("Servidor corriendo http://localhost:3030")})
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'))
