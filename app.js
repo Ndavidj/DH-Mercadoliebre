8a34f441a4c1857d0b5393a4b590cbbd539dbf72
ndavidj <ndavidj@hotmail.com.ar>
2022-04-13 19:54:23 -0300

    subiendo Mercado liebre al repo

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/public")))
+
+app.listen( process.env.PORT || 3030, () => 
+    {console.log("Servidor corriendo")})
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/register.html', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login.html', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
